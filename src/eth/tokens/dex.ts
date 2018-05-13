3b4cc80969b95634352fb6bd8e1c020c2643da7c
MisterDr <djenadrazic@gmail.com>
2018-05-13 19:36:40 +0200

    - Parameter fix

--- src/eth/tokens/dex.ts
+++ src/eth/tokens/dex.ts
@@ -6,6 +6,7 @@ import getAbiParams = AbiUtil.getAbiParams;
 import {EthConfirmation, EthEngine} from "../eth-engine";
 import {ERC20} from "./ERC20";
 import {TokenConfig} from "../../config/tokens/tokenconfig";
+import {TOKENS} from "../../eth-tokens";
 
 
 export class DEX extends ERC20 {
@@ -28,28 +29,29 @@ export class DEX extends ERC20 {
   }
 
   @abiParams({stateMutability: "payable"}, {"": AbiType.uint256}, {"token": AbiType.address}, {"amount": AbiType.uint256})
-  public async DepositToken(token: string, amount: number): Promise<any> {
+  public async DepositToken(amount: number): Promise<any> {
     const abi = getAbiParams(this, "DepositToken");
 
     const configParams = {
       from: this.ethEngine.configuration.defaultWallet
     };
 
-    const result: any = await this.ethEngine.callFunction("DepositToken", [token, amount], configParams, EthConfirmation.CONFIRMATION, abi, TokenConfig.DEX.contractAddress);
+
+    const result: any = await this.ethEngine.callFunction("DepositToken", [this.contractAddress, amount], configParams, EthConfirmation.CONFIRMATION, abi, TokenConfig.DEX.contractAddress);
     return result;
   }
 
 
   @abiParams({stateMutability: "payable"}, {"": AbiType.bool}, {"token": AbiType.address},
     {"amount": AbiType.uint256}, {"nonce": AbiType.uint256}, {"v": AbiType.uint8}, {"r": AbiType.bytes32}, {"s": AbiType.bytes32})
-  public async withdraw(token: string, amount: number, nonce: number, v: string, r: string, s: string): Promise<any> {
+  public async withdraw(amount: number, nonce: number, v: string, r: string, s: string): Promise<any> {
     const abi = getAbiParams(this, "withdraw");
 
     const configParams = {
       from: this.ethEngine.configuration.defaultWallet
     };
 
-    const result: any = await this.ethEngine.callFunction("withdraw", [token, amount], configParams, EthConfirmation.CONFIRMATION, abi, TokenConfig.DEX.contractAddress);
+    const result: any = await this.ethEngine.callFunction("withdraw", [this.contractAddress, amount, nonce, v, r, s], configParams, EthConfirmation.CONFIRMATION, abi, TokenConfig.DEX.contractAddress);
     return result;
   }
 
