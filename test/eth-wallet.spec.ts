4b0279b97170274ae51661f0887d4c2274e340df
MisterDr <djenadrazic@gmail.com>
2018-05-24 09:58:55 +0200

    - Added balance test

--- test/eth-wallet.spec.ts
+++ test/eth-wallet.spec.ts
@@ -1,4 +1,6 @@
 import "jest";
+import {EthereumWallet} from "../src/eth";
+import {TokenFactory, TOKENS} from "../src/eth-tokens";
 
 describe("EthWallet", () => {
   it("Should be pass sanity", () => {
@@ -6,10 +8,23 @@ describe("EthWallet", () => {
     expect(typeof 1).toBe("number");
   });
 
-  // it("Should create new account", () => {
-  //   const ethWallet = new EthWalletTestnet();
-  //   expect(ethWallet.create("customPassword").wallet.privateKey).toBeTruthy();
-  // });
+  it("Should create new account", async () => {
+    const ethWallet = new EthereumWallet();
+
+    const mnemonic = ethWallet.generateMnemonic();
+    const seed = ethWallet.mnemonicToSeed(mnemonic);
+    const account = ethWallet.recoverAccountFromSeed(seed);
+    ethWallet.login(account);
+
+    const token = TokenFactory.GetToken(TOKENS.AUGUR, ethWallet.engine);
+    const result = await token.balanceOf(account.address).then((balance) => {
+      account = Object.assign({}, account, { balance, contractAddress: token.contractAddress, name: TOKENS[TOKENS.AUGUR].toString(), code: 'REP' });
+      return account;
+    });
+
+    console.log(result);
+    expect(true).toBeTruthy();
+  });
   //
   // it("Should be able to login", () => {
   //   const ethWallet = new EthWalletTestnet();
