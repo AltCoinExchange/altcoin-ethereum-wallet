f48ca419611b5a82e2930181ee8cb14dc9aae993
MisterDr <djenadrazic@gmail.com>
2018-07-05 21:12:25 +0200

    - Contract change

--- dist/src/eth-tokens/token-factory.d.ts
+++ dist/src/eth-tokens/token-factory.d.ts
@@ -1,5 +1,5 @@
 import { EthEngine } from "../eth/eth-engine";
-import { AugurTokenMainnet, AugurTokenTestnet } from "./augur";
+import { GolemTokenMainnet } from "./golem";
 export declare enum TOKENS {
     AUGUR = 1,
     GOLEM = 2,
@@ -36,6 +36,6 @@ export declare enum TOKENS {
     WETH = 33,
 }
 export declare class TokenFactory {
-    static GetToken(token: TOKENS, engine: EthEngine, testnet?: boolean): AugurTokenTestnet;
-    static GetTokenMain(token: TOKENS, engine: EthEngine): AugurTokenMainnet;
+    static GetToken(token: TOKENS, engine: EthEngine, testnet?: boolean): GolemTokenMainnet;
+    static GetTokenMain(token: TOKENS, engine: EthEngine): GolemTokenMainnet;
 }
--- src/config/tokens/tokenconfig.ts
+++ src/config/tokens/tokenconfig.ts
@@ -2,7 +2,7 @@
 export const TokenConfig = {
   DEX: {
     code: "",
-    contractAddress: "0x1c4DdFd0F81F3b16bAc27AC5A797B43b52e27634"
+    contractAddress: "0xF2594B0f6762e301d77AD1c9BA673642B3c6b5E5"
   },
   Augur: {
     code: "",
