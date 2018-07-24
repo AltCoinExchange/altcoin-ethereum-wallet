c38f4de365fd8d3acc61d6b5215e7f4b6ca79e2c
azramahmutovic <azramah6@gmail.com>
2018-07-24 23:02:38 +0200

    new test and dist

--- test/eth-wallet.spec.ts
+++ test/eth-wallet.spec.ts
@@ -16,14 +16,22 @@ describe("EthWallet", () => {
     const account = ethWallet.recoverAccountFromSeed(seed);
     ethWallet.login(account);
 
-    // const token = TokenFactory.GetToken(TOKENS.AUGUR, ethWallet.engine);
-    // const result = await token.balanceOf(account.address).then((balance) => {
-    //   account = Object.assign({}, account, { balance, contractAddress: token.contractAddress, name: TOKENS[TOKENS.AUGUR].toString(), code: 'REP' });
-    //   return account;
-    // });
-
-    // console.log(result);
     expect(true).toBeTruthy();
+  });
+
+  it("Should return token balance", async () => {
+    const ethWallet = new EthereumWallet();
+
+    const mnemonic = "glass voyage present gasp silver alpha odor repeat tomorrow elegant input unfold";
+    const seed = ethWallet.mnemonicToSeed(mnemonic);
+    const account = ethWallet.recoverAccountFromSeed(seed);
+    ethWallet.login(account);
+    
+    const token = TokenFactory.GetToken(TOKENS.AUGUR, ethWallet.engine);
+    const result = await token.balanceOf(account.address);
+    const balance = parseFloat(result);
+
+    expect(balance).toBeGreaterThanOrEqual(0);
   });
   //
   // it("Should be able to login", () => {
