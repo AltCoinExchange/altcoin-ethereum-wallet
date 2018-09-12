14a9378b01566a7a8001cd8fdcac269157916576
azramahmutovic <azramah6@gmail.com>
2018-09-12 17:02:22 +0200

    updated ts and created dist

--- dist/src/index.d.ts
+++ dist/src/index.d.ts
@@ -1,3 +1,4 @@
 export * from "./eth/index";
 export { generateMnemonic } from "./hd-bip44/seed";
 export * from "./eth-tokens";
+export * from "./wyre";
---
+++ dist/src/wyre/index.d.ts
@@ -0,0 +1,8 @@
+export declare class WyreVerify {
+    protected web3: any;
+    private contract;
+    private config;
+    private contractAddress;
+    constructor();
+    checkUserStatus(address: any): any;
+}
