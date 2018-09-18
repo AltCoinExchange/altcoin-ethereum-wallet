c19b67f91b2e1c7bb18bdee990e7ca4ebd926057
MisterDr <djenadrazic@gmail.com>
2018-09-18 20:23:46 +0200

    - Changed linter version to reflect new TS 3.0.3

--- src/config/config-eth.ts
+++ src/config/config-eth.ts
@@ -5,12 +5,12 @@ export const EthConfiguration = {
   appkey: "GgZtjS6WPDNAePNM43ZdPoJ1Ap61RU3jnsgjHqELxgHX",
   hosts: [
     {
-      wshost: "wss://swap.altcoin.io:8550",
+      wshost: "wss://geth.altcoin.io",
       contractAddress: "0x6d5ae9dd8f1a2582deb1b096915313459f11ba70",
       defaultWallet: "0xb483f5a8c6ebA74Dc1bcef9D167E6B86Caa4C773",
     },
     {
-      wshost: "wss://swap.altcoin.io:8550",
+      wshost: "wss://geth.altcoin.io",
       contractAddress: "0x6d5ae9dd8f1a2582deb1b096915313459f11ba70",
       defaultWallet: "0x21Fa313f2144f6E1348d91FbF0C6A3a5EEcC056D",
     },
